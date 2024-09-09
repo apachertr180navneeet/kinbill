81cac4c76578ad75ed5b7d9b2657d196ad35437e
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-09-09 15:19:45 +0530

    database changes

---
+++ database/migrations/2024_09_09_094820_add_state_and_zipcode_to_companies_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->string('state')->nullable()->after('address'); // Add state field
+            $table->string('zipcode', 10)->nullable()->after('state'); // Add zipcode field
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->dropColumn(['state', 'zipcode']);
+        });
+    }
+};
