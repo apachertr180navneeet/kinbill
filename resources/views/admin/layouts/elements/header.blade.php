788d54f8cdab6e524f0db46b48bb331dafcbfe71
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-10-05 12:06:35 +0530

    changes

--- resources/views/admin/layouts/elements/header.blade.php
+++ resources/views/admin/layouts/elements/header.blade.php
@@ -22,7 +22,11 @@
 				<a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);"
 					data-bs-toggle="dropdown">
 					<div class="avatar avatar-online">
-						<img src="{{asset('assets/admin/img/avatars/1.png')}}" alt="admin" class="w-px-40 h-auto rounded-circle" />
+						@if(!empty($user->avatar) && file_exists(public_path('/').$user->avatar))
+                            <img src="{{asset($user->avatar)}}" alt="User Image" class="w-px-40 h-auto rounded-circle">
+                        @else
+                            <img src="{{asset('assets/admin/img/avatars/1.png')}}"  alt="User Image" class="w-px-40 h-auto rounded-circle">
+                        @endif
 					</div>
 				</a>
 				<ul class="dropdown-menu dropdown-menu-end">
